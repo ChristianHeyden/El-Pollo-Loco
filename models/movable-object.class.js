4ebbf42e402936fb0f425b22b3b5c11298dd6a50
ChristianHeyden <ch.heyden@outlook.de>
2024-03-10 12:49:36 +0100

    test statusbar

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,16 +1,10 @@
-class MovableObject{
-    x = 120;
-    y = 280;
-    img;
-    height = 150;
-    width = 100;
-    imageCache = [];
-    currentImage = 0;
+class MovableObject extends DrawableObject {
     speed = 0.15;
     otherDirection = false;
     speedY = 0;
     accelaration = 2.5;
     energy = 100;
+    lastHit = 0;
 
     applyGravity(){
         setInterval(() =>{
@@ -25,26 +19,7 @@ class MovableObject{
         return this.y < 120;
     }
 
-
-    loadImage(path){
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-    }
-
-    drawFrame(ctx){
-        if (this instanceof Character || this instanceof Chicken) {
-            ctx.beginPath();
-            ctx.lineWidth = '5';
-            ctx.strokeStyle = 'blue';
-            ctx.rect(this.x, this.y, this.width, this.height);
-            ctx.stroke();
-        }
-
-    }
+    
 
     isColliding(mo){
         return  this.x + this.width > mo.x &&
@@ -57,24 +32,23 @@ class MovableObject{
         this.energy -= 5;
         if (this.energy < 0) {
             this.energy = 0;
+        } else {
+            this.lastHit = new Date().getTime();
         }
+    }
+
+    isHurt(){
+        let timepassed = new Date().getTime() - this.lastHit;
+        timepassed = timepassed / 1000;
+        return timepassed < 1;
     }
 
     isDead(){
         return this.energy == 0;
     }
 
-    
-    loadImages(arr){
-        arr.forEach((path) => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;            
-        });       
-    }
-
     playAnimation(images){
-        let i = this.currentImage % this.IMAGES_WALKING.length; 
+        let i = this.currentImage % images.length; 
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
--- models/world.class.js
+++ models/world.class.js
@@ -8,6 +8,7 @@ class World{
     ctx;
     keyboard;
     camera_x = 0;
+    statusBar = new StatusBar();
 
 
     constructor(canvas, keyboard){
@@ -41,7 +42,7 @@ class World{
         this.ctx.translate(this.camera_x, 0);
         
         this.addObjectsToMap(this.level.backgroundobjects);
-
+        this.addToMap(this.statusBar);
         this.addToMap(this.character);
         this.addObjectsToMap(this.level.clouds);
         this.addObjectsToMap(this.level.enemies);
